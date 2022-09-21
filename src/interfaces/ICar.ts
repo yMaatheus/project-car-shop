7941cc7d84304968b196c3b6b747b1cb09e7132f
Matheus <stvgamer74@gmail.com>
2022-09-21 16:56:17 -0300

    feat: :sparkles: create interface car

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number().int().positive().gte(2)
+    .lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+type ICar = z.infer<typeof CarZodSchema>;
+
+export { ICar, CarZodSchema };
