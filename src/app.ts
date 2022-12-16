beeaabef344550935c179c7a28a170066ef4f6fa
Matheus <stvgamer74@gmail.com>
2022-12-16 15:31:03 -0300

    feat: :sparkles: add integration with swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import error from './middlewares/error.middleware';
 import routes from './routes';
+import doc from '../swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(doc));
 
 app.use(error);
 
